57819386bd190e8a01d4f7b404faa8e853fa2c6f
Youssef1919 <chartaoui1919youssef@gmail.com>
2018-08-03 14:45:27 +0100

    ajax 2

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -221,6 +221,26 @@
 var request;
 
 // Bind to the submit event of our form
+/*
+function verifyEmail()
+{
+
+    if(regex == true)
+        {console.log('valid mail ! ');}
+    else
+        {console.log('invlaid mail ! ');}
+}*/
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    //console.log(pattern.test(emailAddress));
+    if(pattern.test(emailAddress)==true)
+        console.log('true');
+    else 
+        console.log('false');
+}
+$('#email').change(function(){
+    isValidEmailAddress($("#email").val());
+});
 $("#f").submit(function(event){
 
     // Prevent default posting of form - put here to work in case of errors
